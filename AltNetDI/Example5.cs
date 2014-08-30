e988458e90b9d60bc7e268681db1a2797f9e8e9b
Dave Mateer home <davemateer@gmail.com>
2014-08-30 16:58:04 +0100

    Example 5 refactoring

--- AltNetDI/Example5.cs
+++ AltNetDI/Example5.cs
@@ -5,12 +5,10 @@ using Xunit;
 namespace AltNetDI5 {
     class CompositionRoot {
         public static void EMain() {
-            // Instantiating the objects we will need
             IReader reader = new TextFileReader();
-            //IWriter writer = new ConsoleWriter();
 
-            // Decorating ConsoleWriter with a WriterLogger
-            IWriter writer = new WriterLogger(new ConsoleWriter());
+            // Decorating Writer with a WriterLogger
+            IWriter writer = new WriterLogger(new Writer(), new ConsoleWriter(), () => DateTime.Now);
 
             // Pass objects to our Appliction via Dependency Injection
             IApplication application = new Application(reader, writer);
@@ -45,33 +43,45 @@ namespace AltNetDI5 {
     }
 
     public interface IWriter { void Write(string text);}
-    public class ConsoleWriter : IWriter {
+    public class Writer : IWriter {
         public void Write(string text) {
-            // Write to the console
-            Console.WriteLine("ConsoleWriter says: {0}", text);
+            Console.WriteLine("Writer says: {0}", text);
         }
     }
 
     //5. Testing - want to make sure that the datetime outputted of the writerlogger is correct
     public class WriterLogger : IWriter {
         private readonly IWriter writer;
+        private readonly IConsoleWriter consoleWriter;
+        private readonly Func<DateTime> now;
 
-        public WriterLogger(IWriter writer) {
+        public WriterLogger(IWriter writer, IConsoleWriter consoleWriter, Func<DateTime> now)
+        {
             this.writer = writer;
+            this.consoleWriter = consoleWriter;
+            this.now = now;
         }
 
         public void Write(string text) {
-            Console.WriteLine("WriterLogger says: {0} {1}", DateTime.Now, text);
+            consoleWriter.WriteLine("WriterLogger says: {0} {1}", now(), text);
             writer.Write(text);
-            Console.WriteLine("WriterLogger says: Finished logging");
+            consoleWriter.WriteLine("WriterLogger says: Finished logging");
+        }
+    }
+
+    // 
+    public interface IConsoleWriter {void WriteLine(string format, params object[] args);}
+    public class ConsoleWriter : IConsoleWriter {
+        public void WriteLine(string format, params object[] args) {
+            Console.WriteLine(format, args);
         }
     }
 
     public class WriterLoggerTests {
         public void ShouldReturnDateTimeWhichIsNow() {
             var fakeWriter = new FakeWriter();
-            var writerLogger = new WriterLogger(fakeWriter);
-            writerLogger.Write("test message");
+            //var writerLogger = new WriterLogger(fakeWriter);
+            //writerLogger.Write("test message");
 
             //Assert.Equal("WriterLogger says: test message");
         }
