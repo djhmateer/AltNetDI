d65579533ae5c4466d5ee0c3e74deef92a4fd445
Dave Mateer home <davemateer@gmail.com>
2014-08-31 08:44:12 +0100

    Putting in TransformerLogger logic inside there for 2 lines previous and post in Example 8

--- AltNetDI/Example8.cs
+++ AltNetDI/Example8.cs
@@ -17,14 +17,6 @@ namespace AltNetDI8 {
         }
     }
 
-    public interface ISQLTransformerLogger { void Log(string text);}
-    public class SQLTransformerLogger : ISQLTransformerLogger {
-        public void Log(string text) {
-            Console.WriteLine("SQLTransformerLogger says: {0}", text);
-        }
-    }
-
-
     public interface IApplication { void Run();}
     public class Application : IApplication {
         private readonly IReader reader;
@@ -53,11 +45,22 @@ namespace AltNetDI8 {
         }
 
         public string ReplaceOldDbNamesWithNewDbNames(string sql) {
-            // Find where we need to do a replacement
-            var startOfReplace = sql.IndexOf("[Funny]");
-            var sqlToSendToLogger = sql.Substring(startOfReplace - 15, 30);
-            isqlTransformerLogger.Log(sqlToSendToLogger);
+            var positionOfReplace = sql.IndexOf("[Funny]");
+            isqlTransformerLogger.Log(sql, positionOfReplace);
             return sql.Replace("[Funny]", "[FunnyNewDb]");
+        }
+    }
+
+    public interface ISQLTransformerLogger { void Log(string allSQL, int positionOfReplace);}
+    public class SQLTransformerLogger : ISQLTransformerLogger {
+        public void Log(string allSQL, int positionOfReplace) {
+            var message = GetSurroundingTextForReplace(allSQL, positionOfReplace);
+            Console.WriteLine("SQLTransformerLogger says: {0}", message);
+        }
+
+        public string GetSurroundingTextForReplace(string allSQL, int positionOfReplace) {
+            // Could do get preceeding 2 lines and subsequent 2 lines here
+            return allSQL.Substring(positionOfReplace - 15, 30);
         }
     }
 
@@ -78,10 +81,21 @@ namespace AltNetDI8 {
     public class SQLTransformerTests {
         [Fact]
         public void ReplaceOldDbNamesWithNewDbNames_WhenFunny_ShouldReplaceWithFunnyNewDb() {
-            //var transformer = new SQLTransformer();
-            //var result = transformer.ReplaceOldDbNamesWithNewDbNames("[Funny]");
-            //Assert.Equal("[FunnyNewDb]", result);
+            var transformer = new SQLTransformer(new FakeSQLTransformerLogger());
+            var result = transformer.ReplaceOldDbNamesWithNewDbNames("[Funny]");
+            Assert.Equal("[FunnyNewDb]", result);
         }
+
+        [Fact]
+        public void ReplaceOldDbNamesWithNewDbNames_WhenMultipleFunny_ShouldReplaceWithFunnyNewDb() {
+            var transformer = new SQLTransformer(new FakeSQLTransformerLogger());
+            var result = transformer.ReplaceOldDbNamesWithNewDbNames("[Funny] asdf asdf [Funny] asdfasdf");
+            Assert.Equal("[FunnyNewDb] asdf asdf [FunnyNewDb] asdfasdf", result);
+        }
+    }
+
+    public class FakeSQLTransformerLogger : ISQLTransformerLogger {
+        public void Log(string allSQL, int positionOfReplace) { }
     }
 
     //public class FileWriter : IWriter {
